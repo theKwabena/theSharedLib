d26426208cfd1d0658c48f1b3d25f5047e07bd62
theKwabena <abbeyemmanueltetteh@gmail.com>
2024-04-24 00:22:56 +0000

    Added the parameter for the build command

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -1,9 +1,11 @@
 import com.lutermart.BuildParams
 import com.lutermart.Docker
+import com.lutermart.DockerConfig
 
 
-def call(String imageName, String imageTag, Boolean push){
+
+def call(BuildParams params){
     // Takes the imageName, image tag and a boolean push which specifies whether to push
     // the built docker image to a registry
-    return new Docker(this).buildDockerImage(BuildParams params)
+    return new Docker(this).buildDockerImage(params)
 }
