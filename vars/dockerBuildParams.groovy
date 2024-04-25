1eab63969a8c7a9d23e33af71c9cc564b5434383
theKwabena <abbeyemmanueltetteh@gmail.com>
2024-04-25 08:34:37 +0000

    changed the signature to use hashmaps

--- vars/dockerBuildParams.groovy
+++ vars/dockerBuildParams.groovy
@@ -1,11 +1,6 @@
 import com.lutermart.config.DockerBuildParameters
 import com.lutermart.config.DockerConfig
 
-def call(
-        String imageName,
-        String imageTag = 'latest',
-        Boolean push=false,
-        DockerConfig config
-){
-  return new DockerBuildParameters(imageName:imageName, imageTag:imageTag, push:push, config:config)
+def call(Map params){
+  return new DockerBuildParameters(params)
 }
