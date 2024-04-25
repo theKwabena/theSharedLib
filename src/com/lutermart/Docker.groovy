2d97d161287eb19732614c2d664fc92eb6f8e16c
theKwabena <abbeyemmanueltetteh@gmail.com>
2024-04-25 09:19:34 +0000

    changed to old implementataion of docker build and push"

--- src/com/lutermart/Docker.groovy
+++ src/com/lutermart/Docker.groovy
@@ -10,24 +10,24 @@ class Docker implements Serializable{
     }
 
 
-    def buildDockerImage(DockerBuildParameters params){
-        script.echo "----- BUILDING DOCKER IMAGE -----"
-        script.docker.withRegistry(params.config.registry, params.config.credentials_id){
-            def appImage = script.docker.build("$params.imageName:$params.imageTag")
-        }
-
-//        if (params.push) {
-//            script.echo "push set to true, Trying to push docker image to provided repository"
-//            script.docker.withRegistry(params.config.registry, params.config.credentials_id){
-//                appImage.push(params.imageTag)
-//            }
+//    def buildDockerImage(DockerBuildParameters params){
+//        script.echo "----- BUILDING DOCKER IMAGE -----"
+//        def appImage = script.docker.build("$params.imageName:$params.imageTag")
 //
-//        }
-    }
-
-//    def buildDockerImage(String imageName, String imageTag){
-//        script.sh "docker build -t $imageName:$imageTag ."
+//
+////        if (params.push) {
+////            script.echo "push set to true, Trying to push docker image to provided repository"
+////            script.docker.withRegistry(params.config.registry, params.config.credentials_id){
+////                appImage.push(params.imageTag)
+////            }
+////
+////        }
+//        return appImage
 //    }
+
+    def buildDockerImage(DockerBuildParameters params){
+        script.sh "docker build -t $params.imageName:$params.imageTag ."
+    }
 //
 //    def pushDockerImage(image, String registry_url, String credentials_id, String tag = 'latest'){
 //
@@ -37,10 +37,16 @@ class Docker implements Serializable{
 //        script.sh "docker push $imageName"
 //    }
 
-    def pushDockerImage(image, String tag,  String registry, String credentials_id){
+    def pushDockerImage(String image, String registry, String credentials_id){
         script.echo "----- PUSHING DOCKER IMAGE TO $registry -----"
-        script.docker.withRegistry(registry, credentials_id){
-            tag ? image.push(tag) : image.push()
+        script.withCredentials([script.usernamePassword(
+                'credentialsId': credentials_id,
+                'passwordVariable': 'PASS',
+                'usernameVariable': 'USER'
+        )]){
+            script.sh "echo $script.PASS | docker login $registry -u $script.USER --password-stdin"
+            // This defaults to dockerhub if a repo name is not provided
         }
+        script.sh "docker push $registry/$image"
      }
 }
