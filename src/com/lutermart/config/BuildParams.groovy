2a7ce80613f6206aa61362292d75e379243f6421
theKwabena <abbeyemmanueltetteh@gmail.com>
2024-04-25 04:37:23 +0000

    added defaults to buildparams configuration

--- src/com/lutermart/config/BuildParams.groovy
+++ src/com/lutermart/config/BuildParams.groovy
@@ -6,13 +6,13 @@ class BuildParams{
     Boolean push;
     DockerConfig config;
 
-    BuildParams(String imageName, DockerConfig config, String imageTag, Boolean push){
+    BuildParams(String imageName, DockerConfig config, String imageTag='latest', Boolean push =false){
         this.imageName = imageName;
-        this.imageTag = imageTag ?: 'latest';
         this.config = config;
-        this.push = push ?: false;
+        this.imageTag = imageTag
+        this.push = push
 
-        if (push && (config.registry =='' || config.credentials =='' )){
+        if (push && (config.registry =='' || config.credentials_id =='' )){
             throw new IllegalArgumentException("Registry endpoint and credentials must be provided when push is true")
         }
     }
