d367a2e082094fd84b314b9147748bd5f7b81eb2
theKwabena <abbeyemmanueltetteh@gmail.com>
2024-04-30 18:11:38 +0000

    update

--- src/com/lutermart/Nginx.groovy
+++ src/com/lutermart/Nginx.groovy
@@ -30,7 +30,7 @@ class Nginx implements Serializable{
                         returnStdout: true).trim()
             }
 
-            echo "$diffOutput"
+            script.echo "$diffOutput"
 
             if (existingBuildDir == 'not_exists' || diffOutput) {
                 // Move the new build files to the server
